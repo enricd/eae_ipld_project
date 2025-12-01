6fd7c5244f5d7caf349feb492b8239bf208cff7b
enric <edomingodomenech@gmail.com>
2025-12-01 21:22:16 +0100

    change name

--- home.py
+++ home.py
@@ -16,7 +16,7 @@ def home_page():
         st.header("Introduction to Programming Languages for Data")
         st.write("###")
         st.write("***Final Project - Dec 2025***")
-        st.write("**Author:** <Your Name>")
+        st.write("**Author:** Enric Domingo ")
         st.write("**Instructor:** [Enric Domingo](https://github.com/enricd)")
 
 
